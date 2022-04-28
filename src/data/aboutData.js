2d86a74fda62d0d97abdc10da3bae021476d2028
ShyamRavi1 <82603227+ShyamRavi1@users.noreply.github.com>
2022-04-27 23:56:33 -0400

    Updated Code

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "Hello! My name is Shyam Ravichandran. I'm an undergraduate here at Purdue University studying Applied Math and Statistics. In addition to my academic work, I have also been working during Dr. Jung as an Undergraduate Research Assistant in the Department of Physics and Astronomy. My research project revolves around using Deep Neural Networks to study the Standard Model of Particle Physics.",
+    title: "About Me",
+    description1: "Hello! My name is Shyam Ravichandran. I'm an undergraduate here at Purdue University studying Applied Math and Statistics. In addition to my academic work, I have also been working under Dr. Jung as an Undergraduate Research Assistant in the Department of Physics and Astronomy. My research project revolves around using Deep Neural Networks to study the Standard Model of Particle Physics.",
     description2: "A lot of my passions revolve around Math and Computer Science. Recently, I have been building a Neural Network that identify and classifies 156 different letters in my native language of Tamil, and is currently at an accuracy of 87%. Outside of my technical skills and passions, I am a R&B and Hip-Hop Enthusiast, and avid basketball player.",
     image: 2
 }
